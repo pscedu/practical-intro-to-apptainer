1d70a352e66bc678c58e1f427b57960fcc57ea2b
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2024-03-07 00:46:39 -0500

    Added rdogs

---
+++ adventure3/rdogs/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Copyright © 2021-2024 Pittsburgh Supercomputing Center.
+# All Rights Reserved.
+
+IMAGE=apptainer-rdog-1.0.sif
+DEFINITION=recipe.def
+
+apptainer build -F $IMAGE $DEFINITION
